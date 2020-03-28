40397c5017d55f9bb8e0a5b93d78c63ff9e695ad
OlSavMe <olga_savka.os@mail.ru>
2020-03-29 00:00:08 +0200

    router-dom installed

---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./stylesheets/Navigation.scss";
+import { Link } from "react-router-dom";
+
+export default function Navigation() {
+
+    return (
+        <nav>
+            {/* <Link to="/portfolio" className="nav-link">
+                Portfolio
+                    </Link>
+
+            <Link to="/edu" className="nav-link">
+                Education
+                    </Link> */}
+        </nav>
+    );
+}
+
