309a7bf3bb2730450efc12a7677177a3605864aa
OlSavMe <olga_savka.os@mail.ru>
2020-06-08 22:29:14 +0300

    returned to the last but one commit

---
+++ build/precache-manifest.808f629d9e55bcf0e7a6884ff8bee79c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49052afadccff400f85bc5d4c1070f24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51f8ebf90aa0ca9c59fa",
+    "url": "/static/css/main.3df9b1ed.chunk.css"
+  },
+  {
+    "revision": "72ab373d740d71182372",
+    "url": "/static/js/2.6ea158ab.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6ea158ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51f8ebf90aa0ca9c59fa",
+    "url": "/static/js/main.07441fe3.chunk.js"
+  },
+  {
+    "revision": "a323f499b8fdc4cdb657",
+    "url": "/static/js/runtime-main.b6c1e85c.js"
+  },
+  {
+    "revision": "7150bbe1485aad62eff7aef86305290d",
+    "url": "/static/media/at.7150bbe1.png"
+  },
+  {
+    "revision": "e35e5bc36a2d4fc8cad22f95e7464710",
+    "url": "/static/media/backimg.e35e5bc3.jpeg"
+  },
+  {
+    "revision": "67a40b57dce8e58859de5bca16cd769d",
+    "url": "/static/media/marker.67a40b57.png"
+  },
+  {
+    "revision": "e8a72b7d1271fc50b4c64513ff51ed20",
+    "url": "/static/media/ph.e8a72b7d.jpeg"
+  },
+  {
+    "revision": "ccb5abc8102c93d9329ccf00e614fcde",
+    "url": "/static/media/s.ccb5abc8.jpeg"
+  },
+  {
+    "revision": "93640a579d0c742fb3e226ada8b4252c",
+    "url": "/static/media/smob.93640a57.jpeg"
+  }
+]);
